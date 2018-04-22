f81443f54fb37823d8d6b9ac1c69622f48021bb5
Christoph Quinten <christoph.quinten@rwth-aachen.de>
2018-04-23 00:30:46 +0200

    working on including plugin

--- src/main/java/controller/CD4AController.java
+++ src/main/java/controller/CD4AController.java
@@ -390,57 +390,91 @@ public class CD4AController extends AbstractDiagramController {
   
   // Init Buttons
   public void initToolBarActions() {
-    /*
-     * Image image = new Image("/icons/classw.png"); createBtn.setGraphic(new
-     * ImageView(image)); createBtn.setText(""); image = new
-     * Image("/icons/packagew.png"); packageBtn.setGraphic(new
-     * ImageView(image)); packageBtn.setText(""); image = new
-     * Image("/icons/edgew.png"); edgeBtn.setGraphic(new ImageView(image));
-     * edgeBtn.setText(""); image = new Image("/icons/selectw.png");
-     * selectBtn.setGraphic(new ImageView(image)); selectBtn.setText(""); image
-     * = new Image("/icons/undow.png"); undoBtn.setGraphic(new
-     * ImageView(image)); undoBtn.setText(""); image = new
-     * Image("/icons/redow.png"); redoBtn.setGraphic(new ImageView(image));
-     * redoBtn.setText(""); image = new Image("/icons/movew.png");
-     * moveBtn.setGraphic(new ImageView(image)); moveBtn.setText(""); image =
-     * new Image("/icons/deletew.png"); deleteBtn.setGraphic(new
-     * ImageView(image)); deleteBtn.setText(""); image = new
-     * Image("/icons/draww.png"); drawBtn.setGraphic(new ImageView(image));
-     * drawBtn.setText(""); image = new Image("/icons/recow.png");
-     * recognizeBtn.setGraphic(new ImageView(image)); recognizeBtn.setText("");
-     * image = new Image("/icons/micw.png"); voiceBtn.setGraphic(new
-     * ImageView(image)); voiceBtn.setText(""); image = new
-     * Image("/icons/validatew.png"); validateBtn.setGraphic(new
-     * ImageView(image)); validateBtn.setText(""); image = new
-     * Image("/icons/generatew.png"); generateBtn.setGraphic(new
-     * ImageView(image)); generateBtn.setText(""); buttonInUse = createBtn;
-     * buttonInUse.getStyleClass().add("button-in-use"); //
-     * ---------------------- Actions for buttons ----------------------------
-     * createBtn.setOnAction(event -> { tool = ToolEnum.CREATE_CLASS;
-     * setButtonClicked(createBtn); }); packageBtn.setOnAction(event -> { tool =
-     * ToolEnum.CREATE_PACKAGE; setButtonClicked(packageBtn); });
-     * edgeBtn.setOnAction(event -> { tool = ToolEnum.EDGE;
-     * setButtonClicked(edgeBtn); }); selectBtn.setOnAction(event -> { tool =
-     * ToolEnum.SELECT; setButtonClicked(selectBtn); });
-     * drawBtn.setOnAction(event -> { tool = ToolEnum.DRAW;
-     * setButtonClicked(drawBtn); }); moveBtn.setOnAction(event -> {
-     * setButtonClicked(moveBtn); tool = ToolEnum.MOVE_SCENE; });
-     * undoBtn.setOnAction(event -> undoManager.undoCommand());
-     * redoBtn.setOnAction(event -> undoManager.redoCommand());
-     * deleteBtn.setOnAction(event -> deleteSelected());
-     * recognizeBtn.setOnAction(event ->
-     * recognizeController.recognize(selectedSketches));
-     * voiceBtn.setOnAction(event -> { if (voiceController.voiceEnabled) {
-     * Notifications.create().title("Voice disabled").
-     * text("Voice commands are now disabled.").showInformation(); } else {
-     * Notifications.create().title("Voice enabled").
-     * text("Voice commands are now enabled.").showInformation(); }
-     * voiceController.onVoiceButtonClick(); });
-     */
+    
+    Image image = new Image("/icons/classw.png");
+    createBtn.setGraphic(new ImageView(image));
+    createBtn.setText("");
+    image = new Image("/icons/packagew.png");
+    packageBtn.setGraphic(new ImageView(image));
+    packageBtn.setText("");
+    image = new Image("/icons/edgew.png");
+    edgeBtn.setGraphic(new ImageView(image));
+    edgeBtn.setText("");
+    image = new Image("/icons/selectw.png");
+    selectBtn.setGraphic(new ImageView(image));
+    selectBtn.setText("");
+    image = new Image("/icons/undow.png");
+    undoBtn.setGraphic(new ImageView(image));
+    undoBtn.setText("");
+    image = new Image("/icons/redow.png");
+    redoBtn.setGraphic(new ImageView(image));
+    redoBtn.setText("");
+    image = new Image("/icons/movew.png");
+    moveBtn.setGraphic(new ImageView(image));
+    moveBtn.setText("");
+    image = new Image("/icons/deletew.png");
+    deleteBtn.setGraphic(new ImageView(image));
+    deleteBtn.setText("");
+    image = new Image("/icons/draww.png");
+    drawBtn.setGraphic(new ImageView(image));
+    drawBtn.setText("");
+    image = new Image("/icons/recow.png");
+    recognizeBtn.setGraphic(new ImageView(image));
+    recognizeBtn.setText("");
+    image = new Image("/icons/micw.png");
+    voiceBtn.setGraphic(new ImageView(image));
+    voiceBtn.setText("");
+    image = new Image("/icons/validatew.png");
+    validateBtn.setGraphic(new ImageView(image));
+    validateBtn.setText("");
+    image = new Image("/icons/generatew.png");
+    generateBtn.setGraphic(new ImageView(image));
+    generateBtn.setText("");
+    buttonInUse = createBtn;
+    buttonInUse.getStyleClass().add("button-in-use"); //
+    // ---------------------- Actions for buttons ----------------------------
+    createBtn.setOnAction(event -> {
+      tool = ToolEnum.CREATE_CLASS;
+      setButtonClicked(createBtn);
+    });
+    packageBtn.setOnAction(event -> {
+      tool = ToolEnum.CREATE_PACKAGE;
+      setButtonClicked(packageBtn);
+    });
+    edgeBtn.setOnAction(event -> {
+      tool = ToolEnum.EDGE;
+      setButtonClicked(edgeBtn);
+    });
+    selectBtn.setOnAction(event -> {
+      tool = ToolEnum.SELECT;
+      setButtonClicked(selectBtn);
+    });
+    drawBtn.setOnAction(event -> {
+      tool = ToolEnum.DRAW;
+      setButtonClicked(drawBtn);
+    });
+    moveBtn.setOnAction(event -> {
+      setButtonClicked(moveBtn);
+      tool = ToolEnum.MOVE_SCENE;
+    });
+    undoBtn.setOnAction(event -> undoManager.undoCommand());
+    redoBtn.setOnAction(event -> undoManager.redoCommand());
+    deleteBtn.setOnAction(event -> deleteSelected());
+    recognizeBtn.setOnAction(event -> recognizeController.recognize(selectedSketches));
+    voiceBtn.setOnAction(event -> {
+      if (voiceController.voiceEnabled) {
+        Notifications.create().title("Voice disabled").text("Voice commands are now disabled.").showInformation();
+      }
+      else {
+        Notifications.create().title("Voice enabled").text("Voice commands are now enabled.").showInformation();
+      }
+      voiceController.onVoiceButtonClick();
+    });
+    
     // TODO set Actions of new Buttons via CD4APlugin
     validateBtn.setOnAction(event -> {
       // Button was clicked, do something...
-      System.out.println("Funktioniert");
+      Notifications.create().title("Test").text("Funktioniert").showInformation();
     });
   }
   
